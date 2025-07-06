f1fc3bde44c76473f703252dba319cedf5ee5342
Anvith S G <anvithsg2004@gmail.com>
2025-07-06 13:09:17 +0530

    Deployed the backend and updated the URL

--- Frontend/src/api/index.js
+++ Frontend/src/api/index.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
-// const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://135.232.71.98:8085";
-
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8085";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://api-gateway-opyn.onrender.com";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
